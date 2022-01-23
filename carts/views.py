83a65fbca7a923a43247a21c362142ac2ae0ec2b
unknown <awaisashiq0786@gmail.com>
2022-01-23 20:07:41 +0500

    Added Login Functionality and Gitignore file

--- carts/views.py
+++ carts/views.py
@@ -84,7 +84,8 @@ def remove_cart_item(request, product_id, cart_id):
     return redirect('cart')
 
 
-def cart(request, total=0, quantity=0, cart_item=None):
+def cart(request, total=0, quantity=0, cart_items=None):
+    cart_items_count=0
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
--- greate_kart/settings.py
+++ greate_kart/settings.py
@@ -134,3 +134,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.SUCCESS: 'success',
+    messages.ERROR: 'danger',
+}
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'seo.alsultansweets@gmail.com'
+EMAIL_HOST_PASSWORD = '$alsultan.sweets$'
+EMAIL_USE_TLS = True
